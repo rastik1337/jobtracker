01fafee5068639d79d1cd831b5c89f6793564e32
Rastik <38767414+rastik1337@users.noreply.github.com>
2026-04-15 21:57:05 +0200

    rename ViewModel

---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,9 @@
+﻿using JobTracker.Data;
+using JobTracker.Models;
+
+namespace JobTracker.ViewModels;
+
+public partial class MainViewModel(JobTrackerRepository repository) : ViewModelBase
+{
+    private readonly JobTrackerRepository _repository = repository;
+}
--- ViewModels/MainWindowViewModel.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace JobTracker.ViewModels;
-
-public partial class MainWindowViewModel : ViewModelBase
-{
-    public string Greeting { get; } = "Welcome to Avalonia!";
-}
