430806d394bbe63fbc0399c14699a728c219da2f
Rastik <38767414+rastik1337@users.noreply.github.com>
2026-05-05 00:36:05 +0200

    move window content into UserControl

---
+++ Views/Content.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace jobtracker;
+
+public partial class Content : UserControl
+{
+    public Content()
+    {
+        InitializeComponent();
+    }
+}
